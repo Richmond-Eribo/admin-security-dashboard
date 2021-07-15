5405a8206a5e78055ae885b5ba6905c6d9a238df
Richmond Eribo <uyiosaeribo@gmail.com>
2021-07-15 09:55:39 +0100

    components setup

---
+++ data/recruiters.js
@@ -0,0 +1,20 @@
+const Recruiters = [
+  {
+    name: 'TOTAL SUSPECTED LOGIN',
+    number: '30',
+  },
+  {
+    name: 'LOCATION CHANGE ',
+    number: '5',
+  },
+  {
+    name: 'DEVICE CHANGE',
+    number: '10',
+  },
+  {
+    name: 'OTG GENERATED',
+    number: '15',
+  },
+]
+
+export {Recruiters}
---
+++ data/userOvetview.js
@@ -0,0 +1,24 @@
+const Users = [
+  {
+    name: 'TOTAL USERS',
+    number: '1300',
+  },
+  {
+    name: 'RECRUITERS',
+    number: '300',
+  },
+  {
+    name: 'CANDIDATE',
+    number: '1000',
+  },
+  {
+    name: 'ACTIVE RECRUITERS',
+    number: '200',
+  },
+  {
+    name: 'INACTIVE RECRUITES',
+    number: '100',
+  },
+]
+
+export {Users}
